bff3cf54b3c8f433521d8159ddd0b5b0704a628a
Austin Dryden <austinisnorobot@gmail.com>
2019-12-13 11:34:40 -0500

    more updates to strain.js

--- scripts/strain.js
+++ scripts/strain.js
@@ -1,11 +1,11 @@
-let strainRaceObj = {};
-
+let strainName = '';
+let strainID = 1758;
 function strainRaceURLGen(race){
     return `https://strainapi.evanbusse.com/${strainAPIKey}/strains/search/race/${race}`;
 }
 
-function strainURLGen(strainName){
-    return `https://strainapi.evanbusse.com/${strainAPIKey}/strains/search/name/${strainName}`;
+function strainURLGen(strainID){
+    return `https://strainapi.evanbusse.com/${strainAPIKey}/strains/data/effects/${strainID}`;
 }
 
 function lookInside(o){
@@ -21,33 +21,21 @@ function getAllOfRace(race){
 }
 
 function createStrainDOMs(strainsObj){
-    strainsArr = Object.keys(strainsObj);
+    // strainsArr = Object.keys(strainsObj);
     //iterates thru all the strains of a race and creates a clickable DOM DIV for each, style TBD.
+    // onClick function should set both the strainName and strainID global vars
 }
 
-function getStrain(strainName){
-    fetch(strainURLGen(strainName))
+function getStrain(strainID){
+    fetch(strainURLGen(strainID))
         .then(p => p.json())
         .then(lookInside)
         .then(strainToBreedConverter)
 }
 
-
 getAllOfRace('Sativa');
 
-getStrain(`Jamacian`);
-
-
-
-
-
-
-
-
-
-
-
-
+getStrain(strainID);
 
 function strainToBreedConverter(strainObj){
     // some magic goes here.
